cd603ee72c3dfcac94ab9db76a32f96d08bd238c
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-04-17 21:44:57 +0200

    fix build error

--- routes/project.routes.js
+++ routes/project.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const mongoose = require("mongoose");
 
 const Project = require("../models/Project.model");
-const Task = require("./models/Task.model");
+const Task = require("../models/Task.model");
 
 //  POST /api/projects  -  Creates a new project
 router.post("/projects", (req, res, next) => {
